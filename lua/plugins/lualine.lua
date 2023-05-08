af9174d72438bf4cdcb145fe778ead5c5843f2e0
Augusto <augusto.queirantes@gmail.com>
2023-05-08 07:26:22 -0300

    Show entire file path on lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,29 +4,15 @@ require('lualine').setup {
     theme = 'auto',
     component_separators = { left = '', right = ''},
     section_separators = { left = '', right = ''},
-    disabled_filetypes = {
-      statusline = {},
-      winbar = {},
-    },
-    ignore_focus = {},
-    always_divide_middle = true,
-    globalstatus = false,
-    refresh = {
-      statusline = 1000,
-      tabline = 1000,
-      winbar = 1000,
-    }
   },
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch'},
-    lualine_c = {'filename'},
+    lualine_c = {
+      { 'filename', file_status = true, path = 1 }
+    },
     lualine_x = {'filetype'},
     lualine_y = {},
     lualine_z = {}
-  },
-  tabline = {},
-  winbar = {},
-  inactive_winbar = {},
-  extensions = {}
+  }
 }
