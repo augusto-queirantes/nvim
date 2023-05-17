2461584ff1d3c7ebbfbd085c206dfa2ad0d7f41c
Augusto <augusto.queirantes@gmail.com>
2023-05-17 09:52:56 -0300

    Add node installation to setup.sh

--- setup.sh
+++ setup.sh
@@ -10,3 +10,8 @@ rm Hack.zip
 
 # Dependencies
 sudo apt-get install -y ripgrep python3-venv fd-find
+
+# NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+nvm install 18
+npm install -g typescript typescript-language-server
