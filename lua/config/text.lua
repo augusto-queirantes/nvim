1596eb80e815cdf2c186d27a7f6fb37eadfac878
Augusto <augusto.queirantes@gmail.com>
2023-06-09 21:11:18 -0300

    Fix tab

--- lua/config/text.lua
+++ lua/config/text.lua
@@ -4,6 +4,8 @@ local vim_options = vim.o
 vim_options.tabstop = 2
 vim_options.softtabstop = 2
 vim_options.shiftwidth = 2
+vim_options.expandtab = true
+vim_options.smarttab = true
 
 -- Sets line break on 1000 characters
 vim_options.lbr = true
