e3ee5aa4c62a516de1ca769416589fc605916a62
Augusto <augusto.queirantes@gmail.com>
2023-08-17 10:27:03 -0300

    Add coc diagnostic shortcut

--- lua/plugins/coc.lua
+++ lua/plugins/coc.lua
@@ -1,6 +1,4 @@
-local g = vim.g
-
-g.coc_global_extensions = {
+vim.g.coc_global_extensions = {
   'coc-eslint',
   'coc-json',
   'coc-emmet',
@@ -9,6 +7,9 @@ g.coc_global_extensions = {
   'coc-tsserver'
 }
 
+vim.keymap.set("n", "<leader>cd", ":CocDiagnostics<cr>")
+
+-- Coc tab suggestions fix
 local function check_back_space()
   local col = vim.fn.col('.') - 1
   return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
@@ -16,26 +17,30 @@ end
 
 local opts = { silent = true, noremap = true, expr = true, replace_keycodes = false }
 
-vim.keymap.set("i", "<Tab>",
-function()
+vim.keymap.set("i", "<Tab>", function()
   if vim.fn['coc#pum#visible']() == 1 then
     return vim.fn['coc#pum#next'](1)
   end
+
   if check_back_space() then
     return vim.fn['coc#refresh']()
   end
+
   return "<Tab>"
-end
-, opts)
+end, opts)
+
 vim.keymap.set("i", "<S-Tab>", function()
   if vim.fn['coc#pum#visible']() == 1 then
     return vim.fn['coc#pum#prev'](1)
   end
+
   return "<S-Tab>"
 end, opts)
+
 vim.keymap.set("i", "<CR>", function()
   if vim.fn['coc#pum#visible']() == 1 then
     return vim.fn['coc#pum#confirm']();
   end
+
   return "\r"
 end, opts)
