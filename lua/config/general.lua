4f1c6701028afb614e15439b3cb7ded7b385f1d0
Augusto Queirantes <augusto.queirantes@gmail.com>
2023-11-05 17:57:59 -0300

    Imrpove general config

--- lua/config/general.lua
+++ lua/config/general.lua
@@ -1,40 +1,17 @@
 local vim_options = vim.o
 local global_variables = vim.g
 
---
--- Options
---
-
 -- Shows line number
 vim_options.number = true
-
--- Hiddens tabs instead of closing then
---vim_options.hiden = true
 
 -- Allows mouse usage
 vim_options.mouse = "a"
 
--- Shows commands preview in a split
+-- Shows commands preview in a split pane
 vim_options.inccommand = "split"
 
--- Highlights on search
-vim_options.hlsearch = true
-
--- Updates a file when is changed from the outside
-vim_options.autoread = true
-
--- Sets persistent undo
-vim_options.undofile = true
-
--- Allows regex to work properly
+-- Allows regex to work properly when searching
 vim_options.magic = true
-
--- Explicitly disables paste mode
-vim_options.paste = false
-
--- Removes annoting sounds
---vim_options.noerrorbells = true
---vim_options.novisualbell = true
 
 -- Highlights current line
 vim_options.cursorline = true
@@ -42,9 +19,5 @@ vim_options.cursorline = true
 -- Allows ctrl-c usage
 vim_options.clipboard = "unnamedplus"
 
---
--- Globbal Variables
---
-
+-- Leaderkey
 global_variables.mapleader = " "
-global_variables.maplocalleader = " "
