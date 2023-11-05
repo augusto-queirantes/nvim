efff93bb14ca36752e33ba1dfeca51548d4ea040
Augusto Queirantes <augusto.queirantes@gmail.com>
2023-11-05 19:17:28 -0300

    Upgrade telescope version

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -41,7 +41,7 @@ return require("packer").startup(function(use)
 
   -- Telescope
   use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.2',
+    'nvim-telescope/telescope.nvim', tag = '0.1.4',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
 
