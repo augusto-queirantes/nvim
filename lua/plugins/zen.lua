9548dfcc38bc4ebeb4676310021cdbeece3b5b80
Augusto Queirantes <augusto.queirantes@gmail.com>
2024-01-14 12:09:07 -0300

    Add zendmode

---
+++ lua/plugins/zen.lua
@@ -0,0 +1,20 @@
+return {
+  "folke/zen-mode.nvim",
+  opts = {
+    window = {
+      backdrop = 0.95,
+      width = 100,
+      height = 0.8,
+      options = { signcolumn = "no", number = false, cursorline = false },
+    },
+    plugins = {
+      options = { enabled = true, ruler = false, showcmd = false },
+      twilight = { enabled = true },
+      gitsigns = { enabled = false },
+      tmux = { enabled = false },
+    },
+  },
+  config = function()
+    vim.keymap.set("n", "<leader>zm", ":ZenMode<CR>", {})
+  end,
+}
