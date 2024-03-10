8fc7b00f5fa0b9ec77053180cbcfb04059eab683
Augusto Queirantes <augusto.queirantes@gmail.com>
2024-03-10 12:39:14 -0300

    Improve git configuration

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,13 +1,16 @@
 return {
-  {
-    "lewis6991/gitsigns.nvim",
-    lazy = false,
-  },
-  {
-    "tpope/vim-fugitive",
-    config = function()
-      vim.keymap.set("n", "<leader>gs", ":Git<CR>", {})
-      vim.keymap.set("n", "<leader>gd", ":Gvdiffsplit<CR>", {})
-    end,
-  },
+	{
+		"lewis6991/gitsigns.nvim",
+		lazy = false,
+		config = function()
+			require("gitsigns").setup()
+		end,
+	},
+	{
+		"tpope/vim-fugitive",
+		config = function()
+			vim.keymap.set("n", "<leader>gs", ":Git<CR>", {})
+			vim.keymap.set("n", "<leader>gd", ":Gvdiffsplit<CR>", {})
+		end,
+	},
 }
