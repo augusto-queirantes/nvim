3acd9f590a49ea864595870f2da1a32bd28b8520
Augusto Queirantes <augusto.queirantes@gmail.com>
2024-03-09 21:20:17 -0300

    Remove open at startup

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -1,9 +1,3 @@
-local function open_nvim_tree()
-	require("nvim-tree.api").tree.open()
-end
-
-vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
-
 return {
 	"nvim-tree/nvim-tree.lua",
 	version = "*",
