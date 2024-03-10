e2710347287fd82333f8b446769c3adbafb27899
Augusto Queirantes <augusto.queirantes@gmail.com>
2024-03-09 21:56:51 -0300

    Add command to properly close tabs

--- lua/plugins/tabs.lua
+++ lua/plugins/tabs.lua
@@ -1,24 +1,29 @@
 return {
-  {
-    "akinsho/bufferline.nvim",
-    dependencies = "nvim-tree/nvim-web-devicons",
-    config = function()
-      require("bufferline").setup({
-        options = {
-          themable = true,
-          diagnostics = "nvim_lsp",
-          diagnostics_update_in_insert = true,
-          color_icons = true,
-          show_buffer_icons = true,
-          always_show_bufferline = false,
-          indicator = {
-            style = "underline",
-          },
-        },
-      })
-    end,
+	{
+		"akinsho/bufferline.nvim",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+			"famiu/bufdelete.nvim",
+		},
+		config = function()
+			require("bufferline").setup({
+				options = {
+					themable = true,
+					diagnostics = "nvim_lsp",
+					diagnostics_update_in_insert = true,
+					color_icons = true,
+					show_buffer_icons = true,
+					always_show_bufferline = false,
+					indicator = {
+						style = "underline",
+					},
+					close_command = require("bufdelete").bufdelete,
+				},
+			})
+		end,
 
-    vim.keymap.set("n", "<leader>bp", ":BufferLinePick<CR>", {}),
-    vim.keymap.set("n", "<leader>bc", ":BufferLinePickClose<CR>", {}),
-  },
+		vim.keymap.set("n", "<leader>bp", ":BufferLinePick<CR>", {}),
+		vim.keymap.set("n", "<leader>bc", ":BufferLinePickClose<CR>", {}),
+		vim.keymap.set("n", "<C-w>", ":Bdelete<CR>", {}),
+	},
 }
