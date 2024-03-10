fb98ac1c9947574f87c5765f0bc7514e9e929605
Augusto Queirantes <augusto.queirantes@gmail.com>
2024-03-09 21:57:21 -0300

    Improve tree shortcuts

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -1,19 +1,17 @@
 return {
-	"nvim-tree/nvim-tree.lua",
-	version = "*",
-	lazy = false,
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
-	config = function()
-		require("nvim-tree").setup({
-			filters = {
-				dotfiles = true,
-			},
-		})
-	end,
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("nvim-tree").setup({
+      filters = {
+        dotfiles = true,
+      },
+    })
+  end,
 
-	vim.keymap.set("n", "<C->", ":NvimTreeToggle<CR>", {}),
-	vim.keymap.set("n", "<C-n>", ":NvimTreeFocus<CR>", {}),
-	vim.keymap.set("n", "<leader>ff", ":NvimTreeFindFile<CR>", {}),
+  vim.keymap.set("n", "<C-n>", ":NvimTreeFindFile<CR>", {}),
 }
