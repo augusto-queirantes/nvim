86ddcd0769cfae14176a1c988d93b23e4576d8bc
Augusto Queirantes <augusto.queirantes@gmail.com>
2024-04-28 18:37:45 -0300

    Update nonels configuration

---
+++ lua/plugins/nonels.lua
@@ -0,0 +1,66 @@
+return {
+  {
+    "nvimtools/none-ls.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "jay-babu/mason-null-ls.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvimtools/none-ls-extras.nvim",
+    },
+    config = function()
+      local null_ls = require("null-ls")
+      local null_ls_utils = require("null-ls.utils")
+      local mason_null_ls = require("mason-null-ls")
+
+      mason_null_ls.setup({
+        ensure_installed = {
+          "prettier",
+          "stylua",
+          "eslint",
+          "yamllint",
+          "yamlfmt",
+        },
+      })
+
+      local formatting = null_ls.builtins.formatting
+      local diagnostics = null_ls.builtins.diagnostics
+      local code_actions = null_ls.builtins.code_actions
+      local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+      null_ls.setup({
+        root_dir = null_ls_utils.root_pattern(".null-ls-root", "Makefile", ".git", "package.json"),
+
+        sources = {
+          formatting.prettier,
+          formatting.stylua,
+          formatting.yamlfmt,
+
+          diagnostics.eslint,
+          diagnostics.yamllint,
+
+          code_actions.gitsigns,
+        },
+
+        -- Configure format on save
+        on_attach = function(current_client, bufnr)
+          if current_client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+              group = augroup,
+              buffer = bufnr,
+              callback = function()
+                vim.lsp.buf.format({
+                  filter = function(client)
+                    -- Only use null-ls for formatting instead of lsp server
+                    return client.name == "null-ls"
+                  end,
+                  bufnr = bufnr,
+                })
+              end,
+            })
+          end
+        end,
+      })
+    end,
+  },
+}
