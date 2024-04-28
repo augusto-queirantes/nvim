0c53df2d037fe1b023bacf5e90d7b5e89c613029
Augusto Queirantes <augusto.queirantes@gmail.com>
2024-04-28 16:18:38 -0300

    Add some auto commands

---
+++ lua/config/auto-commands.lua
@@ -0,0 +1,18 @@
+local api = vim.api
+
+-- Show cursor line only in active window
+local cursorGrp = api.nvim_create_augroup("CursorLine", { clear = true })
+
+api.nvim_create_autocmd({ "InsertLeave", "WinEnter" }, {
+  pattern = "*",
+  command = "set cursorline",
+  group = cursorGrp,
+})
+
+api.nvim_create_autocmd(
+  { "InsertEnter", "WinLeave" },
+  { pattern = "*", command = "set nocursorline", group = cursorGrp }
+)
+
+-- Resize neovim split when terminal is resized
+vim.api.nvim_command("autocmd VimResized * wincmd =")
