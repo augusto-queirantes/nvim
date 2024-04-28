1210e158a5ffddec85850fa022af5feb361f5020
Augusto Queirantes <augusto.queirantes@gmail.com>
2024-04-28 20:18:26 -0300

    Improve trouble configuration

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -6,39 +6,23 @@ return {
 	lazy = true,
 	keys = {
 		{
-			"<leader>Ll",
+			"<leader>td",
 			"<cmd>Trouble diagnostics toggle<cr>",
 			desc = "Diagnostics (Trouble)",
 		},
 		{
-			"<leader>LL",
-			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-			desc = "Buffer Diagnostics (Trouble)",
-		},
-		{
-			"<leader>xs",
+			"<leader>ts",
 			"<cmd>Trouble symbols toggle focus=false<cr>",
 			desc = "Symbols (Trouble)",
 		},
 		{
-			"<leader>xl",
+			"<leader>tl",
 			"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
 			desc = "LSP Definitions / references / ... (Trouble)",
-		},
-		{
-			"<leader>xL",
-			"<cmd>Trouble loclist toggle<cr>",
-			desc = "Location List (Trouble)",
-		},
-		{
-			"<leader>xQ",
-			"<cmd>Trouble qflist toggle<cr>",
-			desc = "Quickfix List (Trouble)",
 		},
 	},
 	config = function()
 		require("trouble").setup({
-			-- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
 			mode = "workspace_diagnostics",
 			position = "bottom", -- position of the list can be: bottom, top, left, right
 			height = 15,
