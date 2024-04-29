9114a1f1b0f1dc8c4cb35728217471993c7eb64e
Augusto Queirantes <augusto.queirantes@gmail.com>
2024-04-28 21:10:40 -0300

    Start to use neotree

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -1,17 +1,91 @@
 return {
-	"nvim-tree/nvim-tree.lua",
-	version = "*",
-	lazy = false,
+	"nvim-neo-tree/neo-tree.nvim",
 	dependencies = {
+		"nvim-lua/plenary.nvim",
 		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	event = "VeryLazy",
+	keys = {
+		{ "<leader>e", ":Neotree toggle float<CR>", silent = true, desc = "Float File Explorer" },
+		{ "<C-n>", ":Neotree toggle left<CR>", silent = true, desc = "Left File Explorer" },
+		{ "<leader>ff", ":Neotree filesystem reveal left<CR>", silent = true, desc = "Find current file" },
 	},
 	config = function()
-		require("nvim-tree").setup({
-			filters = {
-				dotfiles = false,
+		require("neo-tree").setup({
+			close_if_last_window = true,
+			popup_border_style = "single",
+			enable_git_status = true,
+			enable_modified_markers = true,
+			enable_diagnostics = true,
+			sort_case_insensitive = true,
+			default_component_configs = {
+				indent = {
+					with_markers = true,
+					with_expanders = true,
+				},
+				modified = {
+					symbol = " ",
+					highlight = "NeoTreeModified",
+				},
+				icon = {
+					folder_closed = "",
+					folder_open = "",
+					folder_empty = "",
+					folder_empty_open = "",
+				},
+				git_status = {
+					symbols = {
+						-- Change type
+						added = "",
+						deleted = "",
+						modified = "",
+						renamed = "",
+						-- Status type
+						untracked = "",
+						ignored = "",
+						unstaged = "",
+						staged = "",
+						conflict = "",
+					},
+				},
+			},
+			window = {
+				position = "float",
+				width = 35,
+			},
+			filesystem = {
+				use_libuv_file_watcher = true,
+				filtered_items = {
+					hide_dotfiles = false,
+					hide_gitignored = false,
+					hide_by_name = {
+						"node_modules",
+					},
+					never_show = {
+						".DS_Store",
+						"thumbs.db",
+					},
+				},
+			},
+			event_handlers = {
+				{
+					event = "neo_tree_window_after_open",
+					handler = function(args)
+						if args.position == "left" or args.position == "right" then
+							vim.cmd("wincmd =")
+						end
+					end,
+				},
+				{
+					event = "neo_tree_window_after_close",
+					handler = function(args)
+						if args.position == "left" or args.position == "right" then
+							vim.cmd("wincmd =")
+						end
+					end,
+				},
 			},
 		})
 	end,
-
-	vim.keymap.set("n", "<C-n>", ":NvimTreeFindFile<CR>", {}),
 }
