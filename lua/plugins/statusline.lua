4f7b2867be836fe67d749e973fcad53bd6699e9a
Augusto Queirantes <augusto.queirantes@gmail.com>
2024-04-29 09:33:58 -0300

    Update statuline configuration

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,22 +1,50 @@
 return {
 	"nvim-lualine/lualine.nvim",
+	dependencies = {
+		"meuter/lualine-so-fancy.nvim",
+	},
+	enabled = true,
+	lazy = false,
+	event = { "BufReadPost", "BufNewFile", "VeryLazy" },
 	config = function()
 		require("lualine").setup({
 			options = {
-				icons_enabled = false,
-				theme = "catppuccin",
-				component_separators = { left = "", right = "" },
+				theme = "auto",
+				globalstatus = true,
+				icons_enabled = true,
+				component_separators = { left = "|", right = "|" },
 				section_separators = { left = "", right = "" },
 			},
 			sections = {
-				lualine_a = { "mode" },
-				lualine_b = { "branch" },
-				lualine_c = {
-					{ "filename", file_status = true, path = 1 },
+				lualine_a = {
+					{ "fancy_mode", width = 8 },
 				},
-				lualine_x = { "filetype" },
-				lualine_y = {},
-				lualine_z = {},
+				lualine_b = {
+					"fancy_branch",
+					"fancy_diff",
+				},
+				lualine_c = {
+					{
+						"filename",
+						path = 1,
+						symbols = {
+							modified = "  ",
+						},
+					},
+				},
+				lualine_x = {
+					{
+						"fancy_diagnostics",
+						sources = { "nvim_lsp" },
+						symbols = { error = " ", warn = " ", info = " " },
+					},
+				},
+				lualine_y = {
+					{ "fancy_searchcount" },
+				},
+				lualine_z = {
+					{ "fancy_filetype" },
+				},
 			},
 		})
 	end,
