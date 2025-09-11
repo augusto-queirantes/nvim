a2cc5aca1b70d70b5bf1f6144758bdb633faede3
Augusto Queirantes <augusto.queirantes.dev@gmail.com>
2025-09-11 18:18:16 -0300

    Add properly made ruby configuration

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,4 +1,18 @@
+-- Remove trailing whitespaces
 vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   pattern = { "*" }, -- Apply to all file types
   command = [[%s/\s\+$//e]], -- Substitute trailing whitespace with nothing
+})
+
+-- Standardrb configuration
+-- https://github.com/standardrb/standard/wiki/IDE:-neovim
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "ruby",
+  group = vim.api.nvim_create_augroup("RubyLSP", { clear = true }), -- also this is not /needed/ but it's good practice
+  callback = function()
+    vim.lsp.start({
+      name = "standard",
+      cmd = { "standardrb", "--lsp" },
+    })
+  end,
 })
--- lua/plugins/ruby.lua
+++ lua/plugins/ruby.lua
@@ -7,37 +7,14 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        ruby_lsp = {
-          mason = false,
-          cmd = { "ruby-lsp" },
-          root_dir = function(fname)
-            return require("lspconfig").util.root_pattern("Gemfile", ".git")(fname)
-          end,
+        standardrb = {
+          enabled = formatter == "standardrb",
         },
       },
     },
   },
   {
     "mason-org/mason.nvim",
-    opts = { ensure_installed = { "erb-formatter", "erb-lint" } },
-  },
-  {
-    "stevearc/conform.nvim",
-    optional = true,
-    opts = {
-      formatters_by_ft = {
-        ruby = { "rubocop" },
-        eruby = { "erb_format" },
-      },
-      formatters = {
-        rubocop = {
-          command = "bundle",
-          args = { "exec", "rubocop", "--stdin", "$FILENAME", "--autocorrect-all", "--stderr" },
-          stdin = true,
-          cwd = require("conform.util").root_file("Gemfile"),
-          exit_codes = { 0, 1 },
-        },
-      },
-    },
+    opts = { ensure_installed = { "standardrb" } },
   },
 }
