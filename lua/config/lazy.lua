2b05da8658ae305d13fa0e99979305c078883078
Augusto Queirantes <augusto.queirantes.dev@gmail.com>
2025-10-27 20:26:12 -0300

    Removes useless colorscheme setup

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -32,7 +32,6 @@ require("lazy").setup({
     lazy = false,
     version = false,
   },
-  install = { colorscheme = { "tokyonight", "habamax" } },
   checker = {
     enabled = true,
     notify = false,
