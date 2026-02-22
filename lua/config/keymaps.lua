accf07d8ca41399e19fdb69275f2544f23bd2be2
Augusto Queirantes <augusto.queirantes.dev@gmail.com>
2026-02-22 00:40:54 -0300

    Hard reset config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
 local function map_key(mode, key_to_bind, action_to_execute)
   vim.keymap.set(mode, key_to_bind, action_to_execute)
 end
@@ -9,9 +13,6 @@ map_key("v", "<C-c>", '"+y')
 map_key("i", "<C-v>", "<C-R><C-O>+")
 map_key("v", "<C-v>", '"+p')
 map_key("n", "<C-v>", '"+p')
-
--- Fast quit
-map_key("n", "<C-q>", ":q<CR>")
 
 -- Fast save
 map_key("n", "<C-s>", ":w<CR>")
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,9 +1,5 @@
--- User configuration
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
 
 vim.opt.relativenumber = false
-vim.opt.clipboard = ""
-
--- Ruby configuration
-
-vim.g.lazyvim_ruby_lsp = "ruby_lsp"
-vim.g.lazyvim_ruby_formatter = "rubocop"
